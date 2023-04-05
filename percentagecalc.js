5377e4d458c05313d501bfc0a6c307eb8321d972
Ammar Zahid <126045721+Ammarzahid000@users.noreply.github.com>
2023-04-05 16:07:09 +0500

    Add files via upload

---
+++ percentagecalc.js
@@ -0,0 +1,80 @@
+// alert("How is the Weather today?")
+// prompt("How is the Weather today")
+
+// var a = prompt("How is the Weather today")
+// alert(a)
+// document.write( "today weather is too hot" + a)
+// console.log(a)
+
+// var first_num = +prompt("Your first number")
+// var second_num = +prompt("Your second number")
+// res= first_num + second_num;
+// console.log("your add num is" + res);
+
+// var first_num = +prompt("Your first number")
+// var second_num = +prompt("Your second number")
+// res= first_num - second_num;
+// console.log("your add num is" + res);
+
+
+// var first_num = +prompt("Your first number")
+// var second_num = +prompt("Your second number")
+// res= first_num * second_num;
+// console.log("your add num is" + res);
+
+// document.write("your add number is" + res);
+
+// 3/aprip/2023 class Doing......
+
+// function abc() {
+//     let name = prompt("Enter your name")
+// let tm = +prompt("Enter you total marks")
+// let om = +prompt("Enter you o marks"); 
+// res= om / tm * 100;
+// alert("Hi dear " + name + " your % is " + res);
+// console.log("Hi dear " + name + " your % is " + res);
+// document.write("Hi dear " + name + " your % is " + res);
+    
+// }
+// abc()
+// abc()
+
+// 4/Aprip/2023 Class Work.......
+
+function calc() {
+    let totalMarks_OF_std = +document.querySelector("#stdtm").value;
+
+    let ObtainMarks_OF_std = +document.querySelector("#stdom").value;
+
+    let res = ObtainMarks_OF_std /  totalMarks_OF_std * 100;
+
+    // document.write("You have got it" + res + "%");
+
+    document.querySelector("#ammar").innerHTML= ("your percentage is "     + res   + "%")
+    function fun(){  
+        document.getElementById("myForm").reset();  
+      } 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// var first_num = +prompt("Your first number")
+// var second_num = +prompt("Your second number")
+// res= first_num / second_num;
+// console.log("your add num is" + res);
+// document.write("your add number is" + res);
+// data types
+// " " strings
+// 90239495876 number
+// bolean true or false
+
